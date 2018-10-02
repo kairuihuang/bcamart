8bc452a3d8106d28686d49500abadff2903101a6
kairuiHuang <6kairuih@gmail.com>
2018-10-02 08:52:39 -0400

    set up local server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello world');
+})
+
+app.listen(3003, () => {
+	console.log('Listening on port 3003...');
+})
