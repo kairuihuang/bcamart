92d0dc0db2c0fd3b8c6aa39e74bf3a453e12ed6e
kairuiHuang <6kairuih@gmail.com>
2018-10-25 08:45:36 -0400

    changed port to 4000

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
+const firebase = require('firebase');
 const express = require('express');
 const app = express();
 
-app.get('/hello', (req, res) => {
-	res.send('Hello world');
+const config = {
+  apiKey: "AIzaSyCtyH_NnJVubNiJLycE7dcO_svhpCHQf-8",
+  authDomain: "database-cdf92.firebaseapp.com",
+  databaseURL: "https://database-cdf92.firebaseio.com",
+};
+
+firebase.initializeApp(config);
+const database = firebase.database();
+
+app.set('view engine', 'pug');
+app.set('pages','./pages');
+
+app.get('/', (req, res) => {
+	res.send('Welcome to the root page!');
 });
 
-app.listen(3003, () => {
-	console.log('Listening on port 3003...');
+app.get('/products', (req, res) => {
+	res.render("products");
+});
+
+// const port = process.env.PORT || 4000;
+app.listen(4000, () => {
+	console.log('Listening on port 4000...');
 });
