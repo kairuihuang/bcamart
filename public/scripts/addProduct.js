cd12c91492da03e883de4e40843926c1b0335b57
kairuiHuang <6kairuih@gmail.com>
2019-03-07 08:32:14 -0500

    minor fixes

--- public/scripts/addProduct.js
+++ public/scripts/addProduct.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 	console.log("script loaded");
 	// data validation time!
+	$(".addProductForm").addEventListener("submit", validateAddProduct);
 });
+
+function validateAddProduct () {
+
+}
 
 function isPositiveInteger(n) {
     return n >>> 0 === parseFloat(n);
