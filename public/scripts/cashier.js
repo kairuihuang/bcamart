6d6f2337a621db4709531750cf4e2de66d36b97f
Kairui Huang <6kairuih@gmail.com>
2019-04-11 08:55:56 -0400

    Loading active products only

--- public/scripts/cashier.js
+++ public/scripts/cashier.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 
+    var productList = [];
+
+    $.get('/loadProducts', (data, status) => {
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].isActive) {
+                productList.push(data[i]);
+            }
+        }
+    })
 });
