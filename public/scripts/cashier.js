250e1d72c5dd3c02a4c86252ba451956f73b7552
Kairui Huang <6kairuih@gmail.com>
2019-05-04 00:08:59 -0400

    Subtract item quantities from cart

--- public/scripts/cashier.js
+++ public/scripts/cashier.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
         // event handler when product buttons are clicked
         $('.item').click((event) => {
-            let id = Number(event.currentTarget.id);
+            let id = findProductID(event.target.className);
             let pos = isInCart(cart, id);
 
             // if not in cart yet
@@ -21,7 +21,15 @@ $(document).ready(function(){
                 else { console.log('product not found'); }
             }
             // update quantity and subtotal of exisiting row
-            else { updateCart(id, pos); }
+            else { updateCart(id, pos, true); }
+        });
+
+        // event handler when subtract buttons are clicked
+        $('#cart tbody').on('click', '.subtract', (event) => {
+            let id = findProductID(event.target.className);
+            let pos = isInCart(cart, id);
+            updateCart(id, pos, false);
+            if (cart[pos].quantity === 0) { removeFromCart(id, pos); }
         });
 
         $('#cashBtn').click((event) => {
@@ -57,19 +65,34 @@ function addToCart(info) {
                    <td class='text-left text name'>" + obj.name + "</td>\
                    <td class='text-right number price'>" + obj.price.toFixed(2) + "</td>\
                    <td class='text-right number quantity'>" + obj.quantity + "</td>\
+                   <td class='text-center'>\
+                       <button type='button'\
+                               class='btn btn-sm btn-danger btn-block subtract "
+                               + obj.id + "'>-</button>\
+                   </td>\
                    <td class='text-right number subtotal'>"
                    + obj.subtotal.toFixed(2) + "</td>\
                </tr>";
     $('#cartBody').append(row);
-    updateTotalPrice(obj.price);
+    updateTotalPrice(obj.price, true);
 }
 
-function updateCart(id, pos) {
-    cart[pos].quantity += 1;
+function removeFromCart(id, pos) {
+    $('#' + id).remove();
+    cart.splice(pos, 1);
+}
+
+// if isAdding is true, increment values, decrement if false (subtract an item)
+function updateCart(id, pos, isAdding) {
+    let val;
+    if (isAdding) { val = 1; }
+    else { val = -1; }
+
+    cart[pos].quantity += val;
     cart[pos].subtotal = cart[pos].quantity * cart[pos].price;
     $('#' + id + ' .quantity').text(cart[pos].quantity);
     $('#' + id + ' .subtotal').text(cart[pos].subtotal.toFixed(2));
-    updateTotalPrice(cart[pos].price);
+    updateTotalPrice(cart[pos].price, isAdding);
 }
 
 // returns -1 if not found, else returns product object
@@ -82,8 +105,16 @@ function searchProductByID(list, id) {
     return -1;
 }
 
-function updateTotalPrice(price) {
-    totalPrice += price;
+// finds product ID given string of class names. assumes id is at end string
+function findProductID(classStr) {
+    let arr = classStr.split(' ');
+    return Number(arr[(arr.length)-1]);
+}
+
+function updateTotalPrice(price, isAdding) {
+    let val = price;
+    if (!isAdding) { val *= -1; }
+    totalPrice += val;
     $('#total').val(totalPrice.toFixed(2));
 }
 
@@ -94,8 +125,8 @@ function loadProductBtns(data) {
         }
     }
     for (let i = 0; i < productList.length; i++) {
-        let html = "<button type='secondary' class='btn btn-secondary btn-lg item'\
-                            id='" + productList[i].id + "'>" + productList[i].name;
+        let html = "<button type='button' class='btn btn-secondary btn-lg item " +
+                            productList[i].id + "'>" + productList[i].name;
         $('#pGroup').append(html);
     }
 }
